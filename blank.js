7a47e5cc04d8793f4560bf9aaab318aafd85d527
panqilin <2859439384@qq.com>
2023-01-01 21:37:55 +0800

    add
    add

--- blank.js
+++ blank.js
@@ -1,12 +1,2 @@
-let step = (time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('前端太有趣了！')
-    }, time)
-  })
-}
-async function demo() {
-  let word = await step(500)
-  console.log(word) //前端太有趣了！
-}
-demo()
+let reg = /@qq.com$/
+console.log(reg.test('2859439384@qq.com'))
